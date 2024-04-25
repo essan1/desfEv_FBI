3b7384d2ef5677d172c716c721bf26256bc83095
Esteban Cáceres <estebancacerest@gmail.com>
2024-04-25 16:29:54 -0400

    se finaliza fx en controllers, y se finaliza la ruta del dashboard

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -5,24 +5,40 @@ process.loadEnvFile();
 const secretKey = process.env.SECRET_KEY;
 
 
-const signInControl = (req, res) => {
+export const signIn = (req, res) => {
     try {
         const {email, password } = req.query;
         const agent = agentes.find((agent) => {
         return agent.email === email && agent.password === password;
         })
-        let token = jwt.sign({ email }, secretKey, { expiresIn: "1m"});
+
+        let token = jwt.sign({ email }, secretKey, { expiresIn: "2m"});
+        //validacion
         agent ? res.send(`<p>Agente Autorizado, bienvenido <b>${email}</b>
         Su Token esta en el sessionStorage</p>
         <a href="/dashboard?token=${token}"> Ir al Dashboard</a>
         <script>
             sessionStorage.setItem('token', JSON.stringify("${token}"))
         </script>`) : res.send("Usuario o Password Incorrecta")
+
     } catch (error) {
         console.log(error.message);
     }
 } 
 
-export {
-    signInControl
-}
+
+export const dashboardHome = (req, res) => {
+    const { token } = req.query;
+    try {
+    jwt.verify(token, secretKey,(err,data)=> {
+        if(err){
+            res.status(401).send(`Error: ${err.message}`)
+        } else {res.send(
+          `<center>Bienvenido al <b>DASHBOARD</b> agente. EMAIL: ${data.email}<br><b><a href="/">VOLVER AL HOMEPAGE</a></b></center>`
+        );}
+    });
+    res.send(decoded).status(200);
+  } catch (error) {
+    res.status(401).json({ error: '401 Unathorized', message: error.message });
+  }
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { homePage, rutaGenerica } from "../controllers/web.controllers.js";
-import { signInControl } from "../controllers/user.controllers.js";
+import { signIn, dashboardHome } from "../controllers/user.controllers.js";
 const router = express.Router();
 
 router.get("/", homePage);
-router.get("/signIn", signInControl);
-router.get("/dashboard", );
+router.get("/signIn", signIn);
+router.get("/dashboard", dashboardHome);
 router.get("*", rutaGenerica);
 
 export default router;
