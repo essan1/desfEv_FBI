b3d27c6b7ef8534ee0463b22a384f730fd26e4c1
Esteban Cáceres <estebancacerest@gmail.com>
2024-04-25 11:34:46 -0400

    Incio y Modularizacion del Proyecto

---
+++ src/routes/routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+const router = express.Router();
+const __dirname = import.meta.dirname;
+
+//ruta principal
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../src/views/index.html"));
+});
+
+//creamos nuestra ruta generica, simeprea al final
+router.get("*", (req, res) => {
+  res.status(400);
+  res.send("<h1><center>404 ERROR -- Pagina No Encontrada</center></h1>");
+});
+
+export default router;
