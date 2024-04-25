7c1c1b902aebe123f8067d783b5122117b3f50da
Esteban Cáceres <estebancacerest@gmail.com>
2024-04-25 11:54:20 -0400

    se ajustan detalles esteticos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,11 +2,11 @@ import express from "express";
 import { homePage, rutaGenerica } from "../controllers/controllers.js";
 const router = express.Router();
 
-//ruta principal
-
 router.get("/", homePage);
+//--------------------------
 
-//creamos nuestra ruta generica, simeprea al final
+
+//--------------------------
 router.get("*", rutaGenerica);
 
 export default router;
